18c39b7bd61320487c6d41339267576b484ff95d
Walace Delgado <walacecastro@live.com>
2026-05-25 13:22:54 -0300

    update exercicios

--- 02_Pandas/fila_servico.py
+++ 02_Pandas/fila_servico.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from datetime import datetime
 import matplotlib.pyplot as plt
+
+agora = pd.Timestamp.now()
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 2000)
@@ -13,8 +16,11 @@ fila_servico_cabling = fila_servico_cabling.rename(columns={'#': 'OS'})
 fila_servico_cabling = fila_servico_cabling[
     ['OS', 'Tipo de Registro de Serviço', 'Status', 'Usuário Solicitante', 'Responsável', 'Data de solicitação']]
 
-fila_servico_cabling['Data de solicitação'] = pd.to_datetime(fila_servico_cabling['Data de solicitação'],
-                                                             format='%d-%m-%Y %H:%M:%S')
+# Transformando coluna em datetime
+fila_servico_cabling['Data de solicitação'] = pd.to_datetime(fila_servico_cabling['Data de solicitação'], dayfirst=True, format='%d-%m-%Y %H:%M:%S', errors='coerce')
+
+# Criando coluna de Aging
+fila_servico_cabling['Aging'] = (agora - fila_servico_cabling['Data de solicitação'])
 
 
 # Função para contar quantas vezes cada valor do dataframe aparece
@@ -25,8 +31,23 @@ def verificar_valores_por_quantidade(dataframe, nome_coluna):
 quantidade_por_status = verificar_valores_por_quantidade(fila_servico_cabling, 'Status')
 quantidade_por_responsavel = verificar_valores_por_quantidade(fila_servico_cabling, 'Responsável')
 
+print('\n')
+print(f'Total de chamados na fila: {len(fila_servico_cabling['OS'])}')
+print('\n')
 print(quantidade_por_status)
 print('\n')
 print(quantidade_por_responsavel)
-quantidade_por_responsavel.plot(kind='pie', figsize=(12, 10), autopct='%1.1f%%')
-plt.show()
+print('\n')
+# quantidade_por_responsavel.plot(kind='pie', figsize=(12, 10), autopct='%1.1f%%')
+# plt.show()
+
+print(fila_servico_cabling)
+
+vencidos = len(
+    fila_servico_cabling[
+        fila_servico_cabling['Aging'].dt.days >= 10
+    ]
+)
+
+print(vencidos)
+print('\n')
